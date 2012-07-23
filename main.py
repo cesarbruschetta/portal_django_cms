a9c80f358f1f7994ede57f0bdf991f1dd90aabaf
Cesar Augusto <cesarbruschetta@hotmail.com>
2012-07-23 20:29:54 -0300

    Ajustes para fazer a app funcionar

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-"""WSGI app setup."""
-import os 
-import sys
-import logging
+import os, sys, logging
 
 # Add lib as primary libraries directory, with fallback to lib/dist
 # and optionally to lib/dist.zip, loaded using zipimport.
@@ -15,67 +12,42 @@ if lib_path not in sys.path:
         os.path.join(lib_path, 'dist.zip'),
     ]
 
-from tipfy.app import App
-from config import *
-from urls import rules
-from tipfy.handler import RequestHandler
-from tipfyext.jinja2 import Jinja2Mixin, cached_property
+# Google App Engine imports.
+from google.appengine.ext.webapp import util
 
-#Importing some utilities
-from utils import Resources 
-from config import website_title
-
-def enable_appstats(app):
-    """Enables appstats middleware."""
-    from google.appengine.ext.appstats.recording import \
-        appstats_wsgi_middleware
-    app.dispatch = appstats_wsgi_middleware(app.dispatch)
-
-def enable_jinja2_debugging():
-    """Enables blacklisted modules that help Jinja2 debugging."""
-    if not debug:
-        return
-    from google.appengine.tools.dev_appserver import HardenedModulesHook
-    HardenedModulesHook._WHITE_LIST_C_MODULES += ['_ctypes', 'gestalt']
-
-def dbg():
-    """ Enter pdb in App Engine
-        Renable system streams for it.
-    """
-    import pdb
-    import sys
-    pdb.Pdb(stdin=getattr(sys,'__stdin__'),stdout=getattr(sys,'__stderr__')).set_trace(sys._getframe().f_back)
+from google.appengine.dist import use_library
+use_library('django', '1.3')
 
 
-# Is this the development server?
-debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
-
-# Instantiate the application.
-app = App(rules=rules, config=config, debug=debug)
-enable_appstats(app)
-enable_jinja2_debugging()
+os.environ["DJANGO_SETTINGS_MODULE"] = "mashname.settings"
+sys.path.append("/home/cesaraugusto/workspace/App_Engine/appengine_django")
 
 
-class DefaultHandler(RequestHandler, Jinja2Mixin):
-    """
-    Default class that provides resources and user information handling.
-    """
-    resources = None
-    user_info = None
-    template_vars = {}
-    
-    def __init__(self, request, app=None):
-        
-        super(DefaultHandler, self).__init__(request)
-        self.resources = Resources()
-        self.template_vars = {'resources':self.resources,
-                              'page_title':website_title,
-                              }
 
-        self.context = self.template_vars
+# Force Django to reload its settings.
+#from django.conf import settings
+#settings._target = None
+
+import django.core.handlers.wsgi
+import django.core.signals
+import django.db
+import django.dispatch.dispatcher
+
+# Log errors.
+#django.dispatch.dispatcher.connect(
+#   log_exception, django.core.signals.got_request_exception)
+
+# Unregister the rollback event handler.
+#django.dispatch.dispatcher.disconnect(
+#django.db._rollback_on_exception,
+#django.core.signals.got_request_exception)
 
 def main():
-    app.run()
+    # Create a Django application for WSGI.
+    application = django.core.handlers.wsgi.WSGIHandler()
 
-if __name__ == '__main__':
+    # Run the WSGI CGI handler with that application.
+    util.run_wsgi_app(application)
+
+if __name__ == "__main__":
     main()
